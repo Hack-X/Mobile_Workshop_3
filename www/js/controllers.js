09fcefd7785a0f5a064dd073702189dc1fd2e8c7
Ghislain de Juvigny <gdjuvigny@gmail.com>
2016-09-02 17:09:51 +0200

    workshop X

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,7 +3,10 @@ angular.module('starter.controllers', [])
 .controller('DashCtrl', function($scope) {})
 
 .controller('ShowsCtrl', function($scope, Shows) {
-  $scope.shows = Shows.all();
+  $scope.shows = [];
+  Shows.all().then(function(apiShows) {
+    $scope.shows = apiShows;
+  });
 })
 
 .controller('ShowDetailCtrl', function($scope, $stateParams, Shows) {
