74fddb1202cef682096dd28f378d00fd2ec800b3
Ghislain de Juvigny <gdjuvigny@gmail.com>
2016-09-21 14:59:38 +0200

    add bookings that works

--- www/js/services.js
+++ www/js/services.js
@@ -20,6 +20,12 @@ angular.module('starter.services', [])
         }
       }
       return null;
+    },
+    book: function(showId, user_name, seats) {
+      return $http.post("https://api-shows-tonight.herokuapp.com/shows/" + showId + "/book.json", {booking: {user_name: user_name, seats: seats}}).then(function(response){
+        booking = response.data;
+        return booking;
+      });
     }
   };
 });
